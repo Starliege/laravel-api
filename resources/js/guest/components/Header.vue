eef5a0de0725b696af1405c5bf846c39216bb759
Starliege <urrianif@gmail.com>
2022-10-28 15:15:03 +0200

    first commit

---
+++ resources/js/guest/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>Bentornato JS</div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+    div {
+        font-size: 24px;
+    }
+</style>
---
+++ resources/js/guest/components/Main.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(post, index) in posts" :key="index">
+                <h4>{{ post.title }}</h4>
+                <p>{{ post.content }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Main",
+    data() {
+        return {
+            posts: [],
+        };
+    },
+    created() {
+        axios.get("/api/posts").then((apirisp) => {
+            this.posts = apirisp.data;
+        });
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    li {
+        font-size: 12px;
+    }
+</style>
